ffbc5953cbe07d2ac8675c5e8c10103a95e16dba
Lesia_Nazaruk <nazarullesia@gmail.com>
2025-02-15 20:07:54 +0200

    init

---
+++ src/redux/contactsSlice.js
@@ -0,0 +1,21 @@
+// import { createSlice } from "@reduxjs/toolkit"
+
+
+// const initialState = {
+
+//     contacts: {
+//         items: []
+//     },
+//     filters: {
+//         name: ""
+//     }
+// }
+
+// const slice = createSlice({
+//     name: "contacts",
+//     initialState,
+
+
+// }) 
+
+// export contactsReducer = slice.reducer
---
+++ src/redux/store.js
@@ -0,0 +1,12 @@
+// import { configureStore } from "@reduxjs/toolkit";
+
+
+// export const store = configureStore({
+//     reducer: {
+//         reducers: {
+//             contacts: contactsReducer,
+//             filters: filtersReducer,
+//         },
+
+//     }
+// })
